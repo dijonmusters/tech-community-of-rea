7b0bb8e4a11e70eb5942785424b8d6c2e14f20d9
Jon Meyers <jon.meyers@rea-group.com>
2020-05-22 15:17:18 +1000

    add card fixes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
-import { getDreamJob, getIDE, getIdent, getLanguage, getTheme } from '../../helpers/cardHelper'
 import {
-  Border, InnerCard, Langauge, LangaugeBackground, Name, Image, Stats,
-  IDE, Stat, Title, Bg, BgForReal, StatText, StatSmol, DreamJob, StatIcon, Footer
+  getDreamJob,
+  getIDE,
+  getIdent,
+  getLanguage,
+  getTheme,
+} from '../../helpers/cardHelper'
+import {
+  Border,
+  InnerCard,
+  Langauge,
+  LangaugeBackground,
+  Name,
+  Image,
+  Stats,
+  IDE,
+  Stat,
+  Title,
+  Bg,
+  BgForReal,
+  StatText,
+  StatSmol,
+  DreamJob,
+  StatIcon,
+  Footer,
 } from './Card.style'
 
 const Card = ({ character }) => {
-
-  const profileImage = 'https://avatars2.githubusercontent.com/u/2009724?s=460&u=979eae3bae81ddde1d3f14739b75e8eca7de91b0&v=4'
+  const profileImage = character.imageUrl
   const ide = getIDE(character.ide)
   const indent = getIdent(character.indentWidth)
   const dreamJob = getDreamJob(character.dreamTitle)
@@ -35,26 +55,31 @@ const Card = ({ character }) => {
                 <StatSmol>Element</StatSmol>
                 <span>{ide.text}</span>
               </StatText>
-              <Stat type={ide.type} points={ide.points}>{ide.points}</Stat>
+              <Stat type={ide.type} points={ide.points}>
+                {ide.points}
+              </Stat>
 
               <StatIcon>{indent.icon}</StatIcon>
               <StatText>
                 <StatSmol>Personal space</StatSmol>
                 <span>{character.indentWidth}</span>
               </StatText>
-              <Stat type={indent.type} points={indent.points}>{indent.points}</Stat>
+              <Stat type={indent.type} points={indent.points}>
+                {indent.points}
+              </Stat>
 
               <DreamJob src={dreamJob.icon} />
               <StatText>
                 <StatSmol>Final evolution</StatSmol>
                 <span>{dreamJob.text}</span>
               </StatText>
-              <Stat type={dreamJob.type} points={dreamJob.points}>{dreamJob.points}</Stat>
+              <Stat type={dreamJob.type} points={dreamJob.points}>
+                {dreamJob.points}
+              </Stat>
             </Stats>
 
             <Footer>https://joniscool.com</Footer>
           </Bg>
-
         </InnerCard>
       </Border>
     </ThemeProvider>
